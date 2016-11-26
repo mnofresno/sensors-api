fdc9795813a7bf695ac47323f898032aa31957ad
Mariano Fresno <mnofresno@gmail.com>
2016-11-26 13:25:27 -0300

    Adding the rest of the application files

---
+++ main.js
@@ -0,0 +1,26 @@
+var lm_sensors = require('sensors.js');
+var http       = require('http');
+
+function readSensors(response)
+{
+    lm_sensors.sensors(function (data, error)
+    {
+	if (error) throw error;
+	response.end(JSON.stringify(data));
+    });
+}
+
+const PORT=8922; 
+
+function handleRequest(request, response)
+{
+    response.writeHead(200, {"Content-Type": "application/json"});
+    readSensors(response);
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function()
+{
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
